5cf3a600a4ab1c6872e950384630eb0f2adb42e7
KevinU20221275 <U20221275@univo.edu.sv>
2025-12-23 10:21:05 -0600

    feat: add PackPokemonFactory to handle pokemons of the packs

---
+++ src/main/java/org/kmontano/minidex/factory/PackPokemonFactory.java
@@ -0,0 +1,21 @@
+package org.kmontano.minidex.factory;
+
+import org.kmontano.minidex.domain.pokemon.PackPokemon;
+import org.kmontano.minidex.infrastructure.mapper.PokemonResponse;
+import org.kmontano.minidex.utils.PokemonUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PackPokemonFactory {
+    private final PokemonUtils utils;
+
+    public PackPokemonFactory(PokemonUtils utils) {
+        this.utils = utils;
+    }
+
+    public PackPokemon toPackPokemon(PokemonResponse data, double shinyChance){
+        boolean isShiny = utils.isShiny(shinyChance);
+
+        return new PackPokemon().setName(data.getName()).setImage((utils.selectImage(data, isShiny))).setShiny(isShiny);
+    }
+}
