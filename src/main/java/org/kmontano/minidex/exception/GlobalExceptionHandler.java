b076b8800f4fb4496631f2e75f7c341f156876a0
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-08 12:12:10 -0600

    refactor: update exception handler

--- src/main/java/org/kmontano/minidex/exception/GlobalExceptionHandler.java
+++ src/main/java/org/kmontano/minidex/exception/GlobalExceptionHandler.java
@@ -45,4 +45,18 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("message", "Error interno del servidor"));
     }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalState(IllegalStateException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
 }
