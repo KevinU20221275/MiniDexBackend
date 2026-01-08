d214f0ac9e33ebcd451f263c19c3c8e6ba4cfc1a
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-08 12:01:58 -0600

    refactor: change method to handle open envelope in PokemonStoreService interface

--- src/main/java/org/kmontano/minidex/application/service/TrainerService.java
+++ src/main/java/org/kmontano/minidex/application/service/TrainerService.java
@@ -5,9 +5,12 @@ import org.kmontano.minidex.domain.trainer.Trainer;
 import org.kmontano.minidex.dto.request.AuthRequest;
 import org.kmontano.minidex.dto.request.UpdateCoinsRequest;
 import org.kmontano.minidex.dto.request.UpdateNameAndUsernameRequest;
+import org.kmontano.minidex.dto.response.PackPokemon;
 import org.kmontano.minidex.dto.response.TrainerDTO;
 
+import java.util.List;
 import java.util.Optional;
+
 
 /**
  * Interfaz de servicio para manejar la lógica de negocio de Trainers.
@@ -18,5 +21,5 @@ public interface TrainerService {
     Optional<Trainer> update(Trainer trainer);
     TrainerDTO updateTrainerNameAndUsername(Trainer trainer, UpdateNameAndUsernameRequest request);
     TrainerDTO updateCoinsAndLevel(Trainer trainer, UpdateCoinsRequest request);
-    TrainerDTO openEnvelope(Trainer trainer, String envelopeId);
+    List<PackPokemon> openEnvelope(Trainer trainer);
 }
