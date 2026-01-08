ff9a33691ee781a4eb292ab2b4c0b42a2fbc5292
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-08 12:10:27 -0600

    refactor: update pokedex service

--- src/main/java/org/kmontano/minidex/application/service/PokedexService.java
+++ src/main/java/org/kmontano/minidex/application/service/PokedexService.java
@@ -2,7 +2,9 @@ package org.kmontano.minidex.application.service;
 
 import org.kmontano.minidex.domain.pokedex.Pokedex;
 import org.kmontano.minidex.domain.pokemon.Pokemon;
+import org.kmontano.minidex.dto.response.PackPokemon;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PokedexService {
@@ -13,4 +15,5 @@ public interface PokedexService {
     Optional<Pokedex> addPokemonToTeam(String owner, String pokemonId);
     Optional<Pokedex> removePokemonFromTeam(String owner, String pokemonId);
     Optional<Pokedex> update(Pokedex pokedex);
+    void addPokemonsFromEnvelope(List<PackPokemon> pokemons, String ownerId);
 }
