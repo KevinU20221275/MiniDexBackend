7fbe9aef97752ce2bf39438d058a82e12b972cdc
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-14 17:18:34 -0600

    refactor: update DailyPackService interface

--- src/main/java/org/kmontano/minidex/application/service/DailyPackService.java
+++ src/main/java/org/kmontano/minidex/application/service/DailyPackService.java
@@ -1,17 +1,11 @@
 package org.kmontano.minidex.application.service;
 
 import org.kmontano.minidex.dto.response.PackPokemon;
-import org.kmontano.minidex.domain.trainer.DailyPackStatus;
-import org.kmontano.minidex.domain.trainer.Envelope;
-import org.kmontano.minidex.dto.response.SpecialPokemonDTO;
 
 import java.util.List;
 import java.util.Random;
 
 public interface DailyPackService {
-    DailyPackStatus initialize();
-    List<Envelope> buildEnvelopes(List<PackPokemon> pokemons);
-    DailyPackStatus resetIfNeeded(DailyPackStatus status);
     PackPokemon getRandomPackPokemon();
     PackPokemon getRandomPackPokemon(Double customShinyRate);
     List<PackPokemon> generateDailyPackPokemons();
