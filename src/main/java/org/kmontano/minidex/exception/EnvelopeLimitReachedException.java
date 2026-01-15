04f76f5b342aca5b45f54869a1b9f33d90fa115e
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-14 19:17:09 -0600

    feature: add custom domain exception for open envelope logic

--- src/main/java/org/kmontano/minidex/exception/EnvelopeLimitReachedException.java
+++ src/main/java/org/kmontano/minidex/exception/EnvelopeLimitReachedException.java
@@ -1,4 +1,11 @@
 package org.kmontano.minidex.exception;
 
-public class EnvelopeLimitReachedException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EnvelopeLimitReachedException extends RuntimeException {
+    public EnvelopeLimitReachedException(){
+        super("Daily envelope limit already reached");
+    }
 }
