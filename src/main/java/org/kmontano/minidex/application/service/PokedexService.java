133f8ba8b0f658cc86440046c46b5bbab7f61217
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-15 12:02:57 -0600

    update: update evolPokemon method of PokedexService interface

--- src/main/java/org/kmontano/minidex/application/service/PokedexService.java
+++ src/main/java/org/kmontano/minidex/application/service/PokedexService.java
@@ -2,6 +2,7 @@ package org.kmontano.minidex.application.service;
 
 import org.kmontano.minidex.domain.pokedex.Pokedex;
 import org.kmontano.minidex.domain.pokemon.Pokemon;
+import org.kmontano.minidex.domain.trainer.Trainer;
 import org.kmontano.minidex.dto.response.PackPokemon;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public interface PokedexService {
     Optional<Pokedex> getPokedexByOwner(String owner);
     Optional<Pokedex> addPokemon(String owner, Pokemon pokemon);
     Optional<Pokedex> removePokemon(String owner, String pokemonId);
-    Optional<Pokedex> evolPokemon(String owner, String pokemonId);
+    Optional<Pokemon> evolPokemon(Trainer owner, String pokemonId);
     Optional<Pokedex> addPokemonToTeam(String owner, String pokemonId);
     Optional<Pokedex> removePokemonFromTeam(String owner, String pokemonId);
     Optional<Pokedex> update(Pokedex pokedex);
