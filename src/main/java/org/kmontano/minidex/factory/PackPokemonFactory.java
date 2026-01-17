35905ebf08b41606cf4690b7a656d9e8000209ab
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-16 19:32:20 -0600

    update: update logic for include rarity of pokemon

--- src/main/java/org/kmontano/minidex/factory/PackPokemonFactory.java
+++ src/main/java/org/kmontano/minidex/factory/PackPokemonFactory.java
@@ -1,6 +1,8 @@
 package org.kmontano.minidex.factory;
 
+import org.kmontano.minidex.application.service.EvolutionService;
 import org.kmontano.minidex.dto.response.PackPokemon;
+import org.kmontano.minidex.dto.response.PokemonSpeciesData;
 import org.kmontano.minidex.infrastructure.mapper.PokemonResponse;
 import org.kmontano.minidex.utils.PokemonUtils;
 import org.springframework.stereotype.Component;
@@ -8,14 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class PackPokemonFactory {
     private final PokemonUtils utils;
+    private final EvolutionService evolutionService;
 
-    public PackPokemonFactory(PokemonUtils utils) {
+    public PackPokemonFactory(PokemonUtils utils, EvolutionService evolutionService) {
         this.utils = utils;
+        this.evolutionService = evolutionService;
     }
 
     public PackPokemon toPackPokemon(PokemonResponse data, double shinyChance){
         boolean isShiny = utils.isShiny(shinyChance);
-
-        return new PackPokemon().setName(data.getName()).setImage((utils.selectImage(data, isShiny))).setShiny(isShiny);
+        PokemonSpeciesData speciesData = evolutionService.getSpeciesData(data.getSpecies().getUrl());
+        return new PackPokemon()
+                .setName(data.getName())
+                .setImage((utils.selectImage(data, isShiny)))
+                .setRarity(speciesData.getRarity())
+                .setShiny(isShiny);
     }
 }
