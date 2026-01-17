c83841dc1454f32cfe8f189bd73d7dfad3600b2a
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-16 19:35:29 -0600

    feat: rename and add methods on RewardService interface

--- src/main/java/org/kmontano/minidex/application/service/RewardService.java
+++ src/main/java/org/kmontano/minidex/application/service/RewardService.java
@@ -1,10 +1,12 @@
 package org.kmontano.minidex.application.service;
 
 import org.kmontano.minidex.domain.battle.model.BattleContext;
+import org.kmontano.minidex.domain.pokemon.Pokemon;
 import org.kmontano.minidex.domain.trainer.Trainer;
 import org.kmontano.minidex.dto.shared.BattleReward;
 
-public interface BattleRewardService {
-    BattleReward calculate(BattleContext context);
-    void applyReward(Trainer trainer, BattleReward reward);
+public interface RewardService {
+    BattleReward calculateRewardByBattle(BattleContext context);
+    void applyBattleReward(Trainer trainer, BattleReward reward);
+    int calculateRewardByTransferPokemon(Pokemon pokemon);
 }
