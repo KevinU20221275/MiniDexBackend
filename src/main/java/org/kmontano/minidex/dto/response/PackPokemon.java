24a1fbdf9e63f3fc93e2d2de952422c92a7880ad
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-16 19:27:49 -0600

    refactor: update pack pokemon dto to show rarity

--- src/main/java/org/kmontano/minidex/dto/response/PackPokemon.java
+++ src/main/java/org/kmontano/minidex/dto/response/PackPokemon.java
@@ -1,11 +1,21 @@
 package org.kmontano.minidex.dto.response;
 
+import org.kmontano.minidex.domain.pokemon.Rarity;
+
 public class PackPokemon {
     private String name;
     private String image;
     private boolean shiny;
+    private Rarity rarity;
 
     public PackPokemon() {
+    }
+
+    public PackPokemon(String name, String image, boolean shiny, Rarity rarity) {
+        this.name = name;
+        this.image = image;
+        this.shiny = shiny;
+        this.rarity = rarity;
     }
 
     public String getName() {
@@ -32,6 +42,15 @@ public class PackPokemon {
 
     public PackPokemon setShiny(boolean shiny) {
         this.shiny = shiny;
+        return this;
+    }
+
+    public Rarity getRarity() {
+        return rarity;
+    }
+
+    public PackPokemon setRarity(Rarity rarity) {
+        this.rarity = rarity;
         return this;
     }
 }
