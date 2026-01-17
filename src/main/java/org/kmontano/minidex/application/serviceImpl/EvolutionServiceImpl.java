a127057116623c8a359c3dadbdf56f406418a0ef
KevinU20221275 <U20221275@univo.edu.sv>
2026-01-16 19:34:43 -0600

    feat: implement new methods in EvolutionServiceImpl implementation

--- src/main/java/org/kmontano/minidex/application/serviceImpl/EvolutionServiceImpl.java
+++ src/main/java/org/kmontano/minidex/application/serviceImpl/EvolutionServiceImpl.java
@@ -1,6 +1,9 @@
 package org.kmontano.minidex.application.serviceImpl;
 
 import org.kmontano.minidex.application.service.EvolutionService;
+import org.kmontano.minidex.domain.pokemon.Rarity;
+import org.kmontano.minidex.domain.pokemon.RarityMapper;
+import org.kmontano.minidex.dto.response.PokemonSpeciesData;
 import org.kmontano.minidex.infrastructure.mapper.ChainLink;
 import org.kmontano.minidex.infrastructure.mapper.EvolutionChainResponse;
 import org.kmontano.minidex.infrastructure.mapper.SpeciesResponse;
@@ -18,25 +21,30 @@ public class EvolutionServiceImpl implements EvolutionService {
     }
 
     @Override
-    public Optional<String> getNextEvolutionName(String speciesUrl, String currentName){
-        EvolutionChainResponse evo = getEvolutionChain(speciesUrl);
-        return findNextEvolution(evo.getChain(), currentName);
+    public Optional<String> getNextEvolutionName(SpeciesResponse species, String currentName){
+        EvolutionChainResponse chain = getEvolutionChain(species);
+        return findNextEvolution(chain.getChain(), currentName);
     }
 
     @Override
-    public boolean canEvolve(String speciesUrl, String currentName){
-        return getNextEvolutionName(speciesUrl, currentName).isPresent();
+    public PokemonSpeciesData getSpeciesData(String speciesUrl){
+        SpeciesResponse species = getSpecies(speciesUrl);
+        Rarity rarity = RarityMapper.fromSpecies(species);
+
+        return new PokemonSpeciesData(species, rarity);
     }
 
-    private EvolutionChainResponse getEvolutionChain(String speciesUrl){
-        SpeciesResponse species = restTemplate.getForObject(
-                speciesUrl,
-                SpeciesResponse.class
-        );
-
+    private EvolutionChainResponse getEvolutionChain(SpeciesResponse species){
         return restTemplate.getForObject(
                 species.getEvolution_chain().getUrl(),
                 EvolutionChainResponse.class
+        );
+    }
+
+    private SpeciesResponse getSpecies(String speciesUrl){
+        return restTemplate.getForObject(
+                speciesUrl,
+                SpeciesResponse.class
         );
     }
 
