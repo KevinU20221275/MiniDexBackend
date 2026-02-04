6527961618395d0c6286d0228c6093d5837c89fb
KevinU20221275 <U20221275@univo.edu.sv>
2026-02-03 18:33:52 -0600

    update: fix return methods of  interfaces

--- src/main/java/org/kmontano/minidex/application/service/EvolutionService.java
+++ src/main/java/org/kmontano/minidex/application/service/EvolutionService.java
@@ -1,11 +1,12 @@
 package org.kmontano.minidex.application.service;
 
+import org.kmontano.minidex.domain.pokemon.NextEvolution;
 import org.kmontano.minidex.dto.response.PokemonSpeciesData;
 import org.kmontano.minidex.infrastructure.mapper.SpeciesResponse;
 
 import java.util.Optional;
 
 public interface EvolutionService {
-    Optional<String> getNextEvolutionName(SpeciesResponse species, String currentName);
+    Optional<NextEvolution> getNextEvolution(SpeciesResponse species, String currentName);
     PokemonSpeciesData getSpeciesData(String speciesUrl);
 }
--- src/main/java/org/kmontano/minidex/application/service/PokedexService.java
+++ src/main/java/org/kmontano/minidex/application/service/PokedexService.java
@@ -3,7 +3,9 @@ package org.kmontano.minidex.application.service;
 import org.kmontano.minidex.domain.pokedex.Pokedex;
 import org.kmontano.minidex.domain.pokemon.Pokemon;
 import org.kmontano.minidex.domain.trainer.Trainer;
+import org.kmontano.minidex.dto.response.EvolutionPokemonResponse;
 import org.kmontano.minidex.dto.response.PackPokemon;
+import org.kmontano.minidex.dto.response.TransferPokemonResponse;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +13,8 @@ import java.util.Optional;
 public interface PokedexService {
     Pokedex getPokedexByOwner(String owner);
     Pokedex addPokemon(String owner, Pokemon pokemon);
-    void removePokemon(Trainer trainer, String pokemonId);
-    Pokemon evolPokemon(Trainer owner, String pokemonId);
+    TransferPokemonResponse removePokemon(Trainer trainer, String pokemonId);
+    EvolutionPokemonResponse evolutionPokemon(Trainer owner, String pokemonId);
     Optional<Pokedex> addPokemonToTeam(String owner, String pokemonId);
     void removePokemonFromTeam(String owner, String pokemonId);
     Optional<Pokedex> update(Pokedex pokedex);
