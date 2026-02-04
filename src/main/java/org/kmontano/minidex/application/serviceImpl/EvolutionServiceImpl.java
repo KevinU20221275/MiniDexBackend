968327a33e414e1d3c10bd92ae99c9da215348e1
KevinU20221275 <U20221275@univo.edu.sv>
2026-02-03 18:37:36 -0600

    feat: implement logic to get the next evolution id if exist in EvolutionServiceImpl implementation

--- src/main/java/org/kmontano/minidex/application/serviceImpl/EvolutionServiceImpl.java
+++ src/main/java/org/kmontano/minidex/application/serviceImpl/EvolutionServiceImpl.java
@@ -1,11 +1,13 @@
 package org.kmontano.minidex.application.serviceImpl;
 
 import org.kmontano.minidex.application.service.EvolutionService;
+import org.kmontano.minidex.domain.pokemon.NextEvolution;
 import org.kmontano.minidex.domain.pokemon.Rarity;
 import org.kmontano.minidex.domain.pokemon.RarityMapper;
 import org.kmontano.minidex.dto.response.PokemonSpeciesData;
 import org.kmontano.minidex.infrastructure.mapper.ChainLink;
 import org.kmontano.minidex.infrastructure.mapper.EvolutionChainResponse;
+import org.kmontano.minidex.infrastructure.mapper.Species;
 import org.kmontano.minidex.infrastructure.mapper.SpeciesResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,14 +23,14 @@ public class EvolutionServiceImpl implements EvolutionService {
     }
 
     @Override
-    public Optional<String> getNextEvolutionName(SpeciesResponse species, String currentName){
+    public Optional<NextEvolution> getNextEvolution(SpeciesResponse species, String currentName){
         EvolutionChainResponse chain = getEvolutionChain(species);
         return findNextEvolution(chain.getChain(), currentName);
     }
 
     @Override
     public PokemonSpeciesData getSpeciesData(String speciesUrl){
-        SpeciesResponse species = getSpecies(speciesUrl);
+        SpeciesResponse species = getSpeciesFromApi(speciesUrl);
         Rarity rarity = RarityMapper.fromSpecies(species);
 
         return new PokemonSpeciesData(species, rarity);
@@ -41,33 +43,44 @@ public class EvolutionServiceImpl implements EvolutionService {
         );
     }
 
-    private SpeciesResponse getSpecies(String speciesUrl){
+    private SpeciesResponse getSpeciesFromApi(String speciesUrl){
         return restTemplate.getForObject(
                 speciesUrl,
                 SpeciesResponse.class
         );
     }
 
-    private Optional<String> findNextEvolution(ChainLink chain, String currentName){
+    private Optional<NextEvolution> findNextEvolution(ChainLink chain, String currentName){
         if (chain.getSpecies().getName().equals(currentName)){
             if (!chain.getEvolves_to().isEmpty()){
                 /**
                  * Si existen múltiples evoluciones, se selecciona la primera por defecto.
                  */
+                Species nextSpecies = chain.getEvolves_to().get(0).getSpecies();
+
+                int id = extractIdFromUrl(nextSpecies.getUrl());
                 return Optional.of(
-                        chain.getEvolves_to().get(0).getSpecies().getName()
+                        new NextEvolution(
+                            chain.getEvolves_to().get(0).getSpecies().getName(),
+                            id
+                        )
                 );
             }
             return Optional.empty();
         }
 
         for (ChainLink next : chain.getEvolves_to()){
-            Optional<String> found = findNextEvolution(next, currentName);
+            Optional<NextEvolution> found = findNextEvolution(next, currentName);
             if (found.isPresent()){
                 return  found;
             }
         }
 
         return Optional.empty();
+    }
+
+    private int extractIdFromUrl(String url) {
+        String[] parts = url.split("/");
+        return Integer.parseInt(parts[parts.length - 1]);
     }
 }
