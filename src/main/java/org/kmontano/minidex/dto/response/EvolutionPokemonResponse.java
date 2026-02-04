83f3c0ec526f5c6398c96a98d1365af552edaba1
KevinU20221275 <U20221275@univo.edu.sv>
2026-02-03 18:21:54 -0600

    update: rename files

--- src/main/java/org/kmontano/minidex/dto/response/EvolutionPokemonResponse.java
+++ src/main/java/org/kmontano/minidex/dto/response/EvolutionPokemonResponse.java
@@ -4,16 +4,16 @@ import lombok.Data;
 import org.kmontano.minidex.domain.pokemon.Pokemon;
 
 @Data
-public class EvolPokemonResponse {
+public class EvolutionPokemonResponse {
     private int trainerCoins;
     private int trainerXp;
     private int trainerLevel;
-    private Pokemon evolvedPokemon;
+    private PokemonDTO evolvedPokemon;
 
-    public EvolPokemonResponse(int trainerCoins, int trainerXp, int trainerLevel, Pokemon evolvedPokemon) {
+    public EvolutionPokemonResponse(int trainerCoins, int trainerXp, int trainerLevel, Pokemon evolvedPokemon) {
         this.trainerCoins = trainerCoins;
         this.trainerXp = trainerXp;
         this.trainerLevel = trainerLevel;
-        this.evolvedPokemon = evolvedPokemon;
+        this.evolvedPokemon = new PokemonDTO(evolvedPokemon);
     }
 }
