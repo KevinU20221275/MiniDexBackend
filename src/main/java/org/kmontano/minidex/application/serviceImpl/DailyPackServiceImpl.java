0e4d59d021dcd3d6bcd63d8a0070f76cc0ccc26e
KevinU20221275 <U20221275@univo.edu.sv>
2026-02-08 19:02:06 -0600

    feat: add method to calculate price of special pokemon by rarity and update getPackPokemons method to get pokemons from cache instead of the pokeApi

--- src/main/java/org/kmontano/minidex/application/serviceImpl/DailyPackServiceImpl.java
+++ src/main/java/org/kmontano/minidex/application/serviceImpl/DailyPackServiceImpl.java
@@ -1,10 +1,16 @@
 package org.kmontano.minidex.application.serviceImpl;
 
 import org.kmontano.minidex.application.service.DailyPackService;
+import org.kmontano.minidex.domain.pokemon.Rarity;
+import org.kmontano.minidex.dto.response.DailySpecialPokemonInfoDTO;
 import org.kmontano.minidex.dto.response.PackPokemon;
+import org.kmontano.minidex.exception.ResourceNotFoundException;
 import org.kmontano.minidex.factory.PackPokemonFactory;
+import org.kmontano.minidex.infrastructure.cache.PackPokemonCache;
+import org.kmontano.minidex.infrastructure.cache.PackPokemonCacheRepository;
 import org.kmontano.minidex.infrastructure.mapper.PokemonResponse;
 import org.kmontano.minidex.infrastructure.api.PokemonApiClient;
+import org.kmontano.minidex.infrastructure.mapper.StatSlot;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,10 +27,12 @@ public class DailyPackServiceImpl implements DailyPackService {
 
     private final PokemonApiClient pokeApiClient;
     private final PackPokemonFactory packPokemonFactory;
+    private final PackPokemonCacheRepository cacheRepository;
 
-    public DailyPackServiceImpl(PokemonApiClient pokeApiClient, PackPokemonFactory packPokemonFactory) {
+    public DailyPackServiceImpl(PokemonApiClient pokeApiClient, PackPokemonFactory packPokemonFactory, PackPokemonCacheRepository cacheRepository) {
         this.pokeApiClient = pokeApiClient;
         this.packPokemonFactory = packPokemonFactory;
+        this.cacheRepository = cacheRepository;
     }
 
     @Override
@@ -42,18 +50,18 @@ public class DailyPackServiceImpl implements DailyPackService {
     public PackPokemon getRandomPackPokemon(){
         int randonId = ThreadLocalRandom.current().nextInt(1, 251);
 
-        PokemonResponse response = pokeApiClient.getPokemonById(randonId);
+        PackPokemonCache cache = cacheRepository.findById(randonId).orElseThrow();
 
-        return packPokemonFactory.toPackPokemon(response, SHINY_RATE);
+        return packPokemonFactory.toPackPokemon(cache, SHINY_RATE);
     }
 
     @Override
     public PackPokemon getRandomPackPokemon(Double customShinyRate){
         int randonId = ThreadLocalRandom.current().nextInt(1, 251);
 
-        PokemonResponse response = pokeApiClient.getPokemonById(randonId);
+        PackPokemonCache cache = cacheRepository.findById(randonId).orElseThrow();
 
-        return packPokemonFactory.toPackPokemon(response, customShinyRate);
+        return packPokemonFactory.toPackPokemon(cache, customShinyRate);
     }
 
 
@@ -69,11 +77,32 @@ public class DailyPackServiceImpl implements DailyPackService {
     }
 
     @Override
-    public PackPokemon generateDailySpecial(Random random) {
-        int pokedexNumber = random.nextInt(151) + 1;
+    public DailySpecialPokemonInfoDTO generateDailySpecial(Random random) {
+        List<Integer> ids = cacheRepository.findAll()
+                .stream()
+                .map(PackPokemonCache::getPokemonId)
+                .toList();
 
-        PokemonResponse response = pokeApiClient.getPokemonById(pokedexNumber);
+        int randomId = ids.get(random.nextInt(ids.size()));
 
-        return packPokemonFactory.toPackPokemon(response, 1);
+        PokemonResponse response = pokeApiClient.getPokemonById(randomId);
+
+        PackPokemonCache cache = cacheRepository.findById(randomId).orElseThrow(() -> new ResourceNotFoundException("Pokemon not found"));
+
+        return new DailySpecialPokemonInfoDTO(calculatePriceByRarity(cache.getRarity(), response), packPokemonFactory.toPackPokemon(cache, 1));
+    }
+
+    private int calculatePriceByRarity(Rarity rarity, PokemonResponse data){
+        int base = switch (rarity){
+            case COMMON -> 50;
+            case UNCOMMON -> 75;
+            case EPIC -> 100;
+            case RARE -> 200;
+            case LEGENDARY -> 300;
+        };
+
+        int powerScore = data.getStats().stream().mapToInt(StatSlot::getBaseStat).sum() / 3;
+
+        return base + powerScore;
     }
 }
