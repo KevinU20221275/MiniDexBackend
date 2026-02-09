491bd6cd7dab812d4a0986f00dc07b570ecb4dd6
KevinU20221275 <U20221275@univo.edu.sv>
2026-02-08 19:08:21 -0600

    refactor: update PackPokemonFactory to handle PackPokemonCache instead of PokemonRespose

--- src/main/java/org/kmontano/minidex/factory/PackPokemonFactory.java
+++ src/main/java/org/kmontano/minidex/factory/PackPokemonFactory.java
@@ -1,29 +1,25 @@
 package org.kmontano.minidex.factory;
 
-import org.kmontano.minidex.application.service.EvolutionService;
 import org.kmontano.minidex.dto.response.PackPokemon;
-import org.kmontano.minidex.dto.response.PokemonSpeciesData;
-import org.kmontano.minidex.infrastructure.mapper.PokemonResponse;
+import org.kmontano.minidex.infrastructure.cache.PackPokemonCache;
 import org.kmontano.minidex.utils.PokemonUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PackPokemonFactory {
     private final PokemonUtils utils;
-    private final EvolutionService evolutionService;
 
-    public PackPokemonFactory(PokemonUtils utils, EvolutionService evolutionService) {
+    public PackPokemonFactory(PokemonUtils utils) {
         this.utils = utils;
-        this.evolutionService = evolutionService;
     }
 
-    public PackPokemon toPackPokemon(PokemonResponse data, double shinyChance){
+    public PackPokemon toPackPokemon(PackPokemonCache cache, double shinyChance){
         boolean isShiny = utils.isShiny(shinyChance);
-        PokemonSpeciesData speciesData = evolutionService.getSpeciesData(data.getSpecies().getUrl());
+
         return new PackPokemon()
-                .setName(data.getName())
-                .setImage((utils.selectImage(data, isShiny)))
-                .setRarity(speciesData.getRarity())
+                .setName(cache.getName())
+                .setImage(isShiny ? cache.getImageShiny() : cache.getImageDefault())
+                .setRarity(cache.getRarity())
                 .setShiny(isShiny);
     }
 }
