25b55815a8f27388494db0a7eba44310e8f972d5
KevinU20221275 <U20221275@univo.edu.sv>
2026-02-08 18:59:49 -0600

    update: generateDailySpecial now return a DailySpecialPokemonInfoDTO

--- src/main/java/org/kmontano/minidex/application/service/DailyPackService.java
+++ src/main/java/org/kmontano/minidex/application/service/DailyPackService.java
@@ -1,6 +1,8 @@
 package org.kmontano.minidex.application.service;
 
+import org.kmontano.minidex.dto.response.DailySpecialPokemonInfoDTO;
 import org.kmontano.minidex.dto.response.PackPokemon;
+import org.kmontano.minidex.infrastructure.mapper.PokemonResponse;
 
 import java.util.List;
 import java.util.Random;
@@ -10,5 +12,5 @@ public interface DailyPackService {
     PackPokemon getRandomPackPokemon(Double customShinyRate);
     List<PackPokemon> generateDailyPackPokemons();
     List<PackPokemon> getPokemonsFromBoostedPack();
-    PackPokemon generateDailySpecial(Random random);
+    DailySpecialPokemonInfoDTO generateDailySpecial(Random random);
 }
